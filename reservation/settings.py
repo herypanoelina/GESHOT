c51c49679fe4d57950518c1135fb2929a9c6561b
herypanoelina <74909321+herypanoelina@users.noreply.github.com>
2021-01-15 12:36:54 +0300

    Update settings.py

--- reservation/settings.py
+++ reservation/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import dj_database_url
+#import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,13 +87,13 @@ WSGI_APPLICATION = 'reservation.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    print("Postgres URL not found, using sqlite instead")
-    DATABASES = {
+#if 'DATABASE_URL' in os.environ:
+#   DATABASES = {
+#       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#   }
+#else:
+#    print("Postgres URL not found, using sqlite instead")
+  DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
