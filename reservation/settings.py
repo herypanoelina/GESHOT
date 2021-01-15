eac396d26cc75c86c1fd3e7c58faa0c963e87d7e
herypanoelina <74909321+herypanoelina@users.noreply.github.com>
2021-01-15 12:51:59 +0300

    Update settings.py

--- reservation/settings.py
+++ reservation/settings.py
@@ -86,19 +86,14 @@ WSGI_APPLICATION = 'reservation.wsgi.application'
 
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-#if 'DATABASE_URL' in os.environ:
-#   DATABASES = {
-#       'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#   }
-#else:
-#    print("Postgres URL not found, using sqlite instead")
-  DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
