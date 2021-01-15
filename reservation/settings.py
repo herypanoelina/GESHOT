ef37ecf4cfce52c4bb5dfb2d4f9d372dfab94be0
herypanoelina <74909321+herypanoelina@users.noreply.github.com>
2021-01-15 15:26:46 +0300

    Update settings.py

--- reservation/settings.py
+++ reservation/settings.py
@@ -147,3 +147,19 @@ if os.environ.get('ENV') == 'PRODUCTION':
     )
 
 # ...
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
